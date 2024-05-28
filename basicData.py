001a0bbdaf621b2340ac1fecb9ef4ebb0fcb033d
Shiyu He <shiyu.he@happyelements.com>
2024-05-28 19:46:57 +0800

    fix: add list

---
+++ basicData.py
@@ -0,0 +1,23 @@
+# 基础数据类型[int/float/bool/str]
+
+# int 整数
+
+# float 小数
+
+# bool bool数
+
+# str 字符串
+
+
+# 判断数据类型 type()
+
+print(type('你干嘛捏'))
+
+
+# 数据类型进行转换
+
+y = 122
+x = str(y)
+
+print(type(y))
+print(type(x))
---
+++ helloWorld.py
@@ -0,0 +1,17 @@
+print("This line will be printed .")
+
+
+x = 1
+if x == 1:
+    # indented four spaces
+    print("x is 1")
+'''
+这是多行注释
+'''
+# 这是单行注释
+
+print("鸡你太美1")
+# print("鸡你太美2")
+# print("鸡你太美3")
+# print("鸡你太美4")
+print("鸡你太美5")
